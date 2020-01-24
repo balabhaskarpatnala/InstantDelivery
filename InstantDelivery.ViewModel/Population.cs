37f646105273602046d7a70b17bf77ffb4632c1f
Satya Kalasapati <yuvasena.123@gmail.com>
2020-01-24 12:39:35 +0530

    ok

---
+++ InstantDelivery.ViewModel/Population.cs
@@ -0,0 +1,16 @@
+﻿using PropertyChanged;
+
+namespace InstantDelivery.ViewModel
+{
+    /// <summary>
+    /// Pomocnicza klasa do konstrukcji wykresu
+    /// </summary>
+    //[ImplementPropertyChanged]
+    [AddINotifyPropertyChangedInterface]
+    public class Population
+    {
+        public string Name { get; set; } = string.Empty;
+
+        public int Count { get; set; }
+    }
+}
---
+++ InstantDelivery.ViewModel/ShowShellEvent.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace InstantDelivery.ViewModel
+{
+    public class ShowShellEvent
+    {
+        public Type ViewModel { get; private set; }
+
+        public ShowShellEvent(Type viewModel)
+        {
+            ViewModel = viewModel;
+        }
+    }
+}
